0b878e07541381d4dd8b143a4f12941a747c132e
DB823 <63707797+DB823@users.noreply.github.com>
2021-05-17 00:45:57 +1000

    Added Lasers. Currently lasers are being slow, will continue after they hit the object and are vertical

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -14,6 +14,12 @@ public class GunController : MonoBehaviour
 
     private float damage = 10f;
 
+    public GameObject projectile;
+
+    public GameObject gun;
+
+    public float speed = 100000000f;
+
     void Update()
     {
         shoot = Input.GetKeyDown(KeyCode.Mouse0);
@@ -28,10 +34,12 @@ public class GunController : MonoBehaviour
         Vector3 direction = transform.forward;
         RaycastHit hit = default;
         Vector3 localOffset = transform.position + (transform.up * 2);
+        GameObject instBullet = Instantiate(projectile, gun.transform.position, Quaternion.identity) as GameObject;
+        Rigidbody instBulletRigidbody = instBullet.GetComponent<Rigidbody>();
+        instBulletRigidbody.AddForce(cam.transform.forward * speed);
         if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, Mathf.Infinity))
         {
             hit.collider.SendMessageUpwards("EnergyLoss", 2, SendMessageOptions.DontRequireReceiver);
-
         }
         gameObject.GetComponent<EnergyManager>().EnergyLoss(1);
         shoot = false;
